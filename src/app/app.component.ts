0ca98f7944d6e1f57be9e5c826a1b45fbecdffde
loicbefve <befve.loic@gmail.com>
2019-02-20 13:05:12 +0100

    First commit
    Signed-off-by: loicbefve <befve.loic@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyCDBgRAgwzoHSDYaAYCY9-T1oe8-PBdlRA',
+      authDomain: 'bookshelves-76041.firebaseapp.com',
+      databaseURL: 'https://bookshelves-76041.firebaseio.com',
+      projectId: 'bookshelves-76041',
+      storageBucket: '',
+      messagingSenderId: '616285128381'
+    };
+    firebase.initializeApp(config);
+  }
+
   title = 'bookshelves';
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
+import {AuthService} from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent implements OnInit {
+
+  isAuth: boolean;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+  }
+
+}
